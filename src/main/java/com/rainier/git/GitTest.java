3796e6f01aba4fc0e74848be9aa8934bd6c4df23
Rainier <815609947@rainier.com>
2023-03-30 13:43:34 +0800

    first  commit

---
+++ src/main/java/com/rainier/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.rainier.git;
+
+/**
+ * @author cook
+ * @date 2023/3/30 13:40
+ * @email 3293336923@qq.com
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
