09df4ad720e830e483b438c49ecfd44feee40543
Rainier <815609947@rainier.com>
2023-03-30 18:54:49 +0800

    singleton

---
+++ src/main/java/com/rainier/git/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.rainier.git;
+
+/**
+ * @author cook
+ * @date 2023/3/30 18:34
+ * @email 3293336923@qq.com
+ * @Description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
