f32f0bc323871917e3211ff2d8039f36266129bd
Rainier <815609947@rainier.com>
2023-03-30 15:37:36 +0800

    hot-fix commit2

--- src/main/java/com/rainier/git/GitTest1.java
+++ src/main/java/com/rainier/git/GitTest1.java
@@ -12,5 +12,6 @@ public class GitTest1 {
         System.out.println("heloworld and  git2");
         System.out.println("heloworld and  git3");
         System.out.println("hello  git4");
+        System.out.println("hot-fix  test");
     }
 }
