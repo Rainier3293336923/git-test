b0f6c028419f00c4cef8a9535e70208b8574f41b
Rainier <815609947@rainier.com>
2023-04-01 09:43:03 +0800

    test1    commit

--- src/main/java/com/rainier/git/GitTest.java
+++ src/main/java/com/rainier/git/GitTest.java
@@ -2,12 +2,12 @@ package com.rainier.git;
 
 /**
  * @author cook
- * @date 2023/3/30 18:34
+ * @date 2023/4/1 9:33
  * @email 3293336923@qq.com
  * @Description
  */
-public class HelloWorld {
+public class GitTest {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+
     }
 }
